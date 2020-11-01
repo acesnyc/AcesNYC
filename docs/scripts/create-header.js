26dfb45a950ac6474fac6e706dafc725acd318fe
katthartic <katt.baum@gmail.com>
2020-11-01 15:54:52 -0500

    start drying

---
+++ docs/scripts/create-header.js
@@ -0,0 +1,38 @@
+let main = document.querySelector('#main')
+
+const headerImg = {
+    url: "ACES-NYC-square.jpg",
+    altText: "Aces NYC",
+    width: 200,
+    height: 200
+}
+
+const navList = [
+    { key: "home", title: "Home", page: "index.html" },
+    { key: "home", title: "Events", page: "events.html" },
+    { key: "home", title: "Home", page: "index.html" },
+    { key: "home", title: "Home", page: "index.html" },
+
+]
+
+const headerBar = document.querySelector('#header-and-nav')
+
+const createHeaderAndNav = (image, list) => {
+}
+
+/*
+
+<div id="header-container" class="container">
+<a href="index.html">
+    <img src="ACES-NYC-square.jpg" alt="Aces NYC" itemprop="logo" width="200" height="200" /></a>
+</div> <!-- header-container-->
+<div id='nav-container' class="container">
+<ul id="nav-list">
+    <li class="nav-list-item"><a href="index.html">Home</a></li>
+    <li class="nav-list-item"><a href="events.html">Events</a></li>
+    <li class="nav-list-item"><a href="resources.html">Ace Resources</a></li>
+    <li class="nav-list-item"><a href="contact.html">Contact Us</a></li>
+</ul> <!-- nav-list -->
+</div> <!-- nav-container -->
+
+*/
